d5b570434531fd60131352b541ea1f03efe0faab
EmilyLow27 <1211103315@student.mmu.edu.my>
2024-07-12 22:55:34 +0800

    Update LoadingBar.js

--- libs/LoadingBar.js
+++ libs/LoadingBar.js
@@ -6,32 +6,34 @@ class LoadingBar {
 		this.domElement.style.left = '0';
 		this.domElement.style.width = '100%';
 		this.domElement.style.height = '100%';
-		this.domElement.style.background = '#000';
+		this.domElement.style.background = '#000'; // New background color
 		this.domElement.style.opacity = '0.7';
 		this.domElement.style.display = 'flex';
 		this.domElement.style.alignItems = 'center';
 		this.domElement.style.justifyContent = 'center';
 		this.domElement.style.zIndex = '1111';
-
 		const barBase = document.createElement("div");
-		barBase.style.background = '#FF0000';
+		barBase.style.background = '#aaa';
 		barBase.style.width = '50%';
 		barBase.style.minWidth = '250px';
 		barBase.style.borderRadius = '10px';
 		barBase.style.height = '15px';
 		this.domElement.appendChild(barBase);
-
 		const bar = document.createElement("div");
 		bar.style.background = '#22a';
 		bar.style.width = '50%';
 		bar.style.borderRadius = '10px';
 		bar.style.height = '100%';
 		bar.style.width = '0';
-		bar.style.transition = 'width 0.5s ease'; // Add transition for animation
 		barBase.appendChild(bar);
 		this.progressBar = bar;
 
 		document.body.appendChild(this.domElement);
+
+		function onprogress(delta) {
+			const progress = delta * 100;
+			loader.progressBar.style.width = `${progress}%`;
+		}
 	}
 
 	set progress(delta) {
