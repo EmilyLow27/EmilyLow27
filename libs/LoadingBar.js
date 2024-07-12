e9b383083e9d6158abf982e184d9371408d1461f
EmilyLow27 <1211103315@student.mmu.edu.my>
2024-07-12 22:21:36 +0800

    Update LoadingBar.js

--- libs/LoadingBar.js
+++ libs/LoadingBar.js
@@ -7,7 +7,7 @@ class LoadingBar{
 		this.domElement.style.width = '100%';
 		this.domElement.style.height = '100%';
 		this.domElement.style.background = '##0000FF';
-		this.domElement.style.opacity = '0.7';
+		this.domElement.style.opacity = '0.0';
 		this.domElement.style.display = 'flex';
 		this.domElement.style.alignItems = 'center';
 		this.domElement.style.justifyContent = 'center';
