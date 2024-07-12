9b1e2985401f0de7b471f1740726834858e0a082
EmilyLow27 <1211103315@student.mmu.edu.my>
2024-07-12 22:13:24 +0800

    Update LoadingBar.js

--- libs/LoadingBar.js
+++ libs/LoadingBar.js
@@ -20,7 +20,7 @@ class LoadingBar{
 		barBase.style.height = '15px';
 		this.domElement.appendChild(barBase);
 		const bar = document.createElement("div");
-		bar.style.background = '#ADD8E6';
+		bar.style.background = '#1E90FF';
 		bar.style.width = '50%';
 		bar.style.borderRadius = '10px';
 		bar.style.height = '100%';
