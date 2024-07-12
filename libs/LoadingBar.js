b0c9758aec300d6f2a16c3e2e80f89128d46ebfb
EmilyLow27 <1211103315@student.mmu.edu.my>
2024-07-12 22:06:12 +0800

    Update LoadingBar.js

--- libs/LoadingBar.js
+++ libs/LoadingBar.js
@@ -1,17 +1,18 @@
-class LoadingBar{
-	constructor(options){
+class LoadingBar {
+	constructor(options) {
 		this.domElement = document.createElement("div");
 		this.domElement.style.position = 'fixed';
 		this.domElement.style.top = '0';
 		this.domElement.style.left = '0';
 		this.domElement.style.width = '100%';
 		this.domElement.style.height = '100%';
-		this.domElement.style.background = '#000';
+		this.domElement.style.background = '#ff0000'; // Red background color
 		this.domElement.style.opacity = '0.7';
 		this.domElement.style.display = 'flex';
 		this.domElement.style.alignItems = 'center';
 		this.domElement.style.justifyContent = 'center';
 		this.domElement.style.zIndex = '1111';
+
 		const barBase = document.createElement("div");
 		barBase.style.background = '#aaa';
 		barBase.style.width = '50%';
@@ -19,6 +20,7 @@ class LoadingBar{
 		barBase.style.borderRadius = '10px';
 		barBase.style.height = '15px';
 		this.domElement.appendChild(barBase);
+
 		const bar = document.createElement("div");
 		bar.style.background = '#22a';
 		bar.style.width = '50%';
@@ -27,26 +29,17 @@ class LoadingBar{
 		bar.style.width = '0';
 		barBase.appendChild(bar);
 		this.progressBar = bar;
-		
+
 		document.body.appendChild(this.domElement);
-		
-		function onprogress(delta){
-			const progress = delta*100;
-			loader.progressBar.style.width = `${progress}%`;
-		}
 	}
-	
-	set progress(delta){
-		const percent = delta*100;
+
+	set progress(delta) {
+		const percent = delta * 100;
 		this.progressBar.style.width = `${percent}%`;
 	}
-	
-	set visible(value){
-		if (value){
-			this.domElement.style.display = 'flex';
-		}else{
-			this.domElement.style.display = 'none';
-		}
+
+	set visible(value) {
+		this.domElement.style.display = value ? 'flex' : 'none';
 	}
 }
 
